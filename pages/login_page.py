817be93f5991ce72e4eec8b3379c1c3e88e3a9e4
adam-eden <andrei1foto@gmail.com>
2022-04-15 16:30:01 +0600

    delete comments

--- pages/login_page.py
+++ pages/login_page.py
@@ -12,9 +12,7 @@ class LoginPage(BasePage):
         assert "login" in self.browser.LoginPageLocators.LOGIN_URL, "Login URL is not presented"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"

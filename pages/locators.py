6bc3e8c1dcd56a27759ec0d1480281bc8a34e373
adam-eden <andrei1foto@gmail.com>
2022-05-06 21:15:26 +0600

    Add ProductPage locators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,13 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    MESSAGE_ADD = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
+    MESSAGE_PRICE = (By.CSS_SELECTOR, "#messages p:nth-child(1) > strong")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "#content_inner .price_color")
+
+
+

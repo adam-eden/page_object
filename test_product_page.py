4f76845b594622183aa27216624ee920a9871633
adam-eden <andrei1foto@gmail.com>
2022-05-06 21:16:55 +0600

    Create product page tests

---
+++ test_product_page.py
@@ -0,0 +1,18 @@
+from pages.product_page import ProductPage
+import time
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    time.sleep(2)
+    page.should_be_message_add_product()
+    time.sleep(2)
+    page.should_be_message_total_price()
+    time.sleep(2)
+
+
+

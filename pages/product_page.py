e3443725abbe37aec0eb243c362649b9b7e959cb
adam-eden <andrei1foto@gmail.com>
2022-05-07 17:31:07 +0600

    Optimized checks and added negative

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,13 +8,18 @@ class ProductPage(BasePage):
         button.click()
 
     def should_be_message_add_product(self):
-        assert "Coders at Work" == self.browser.find_element(*ProductPageLocators.MESSAGE_ADD).text, \
-            "Message about adding product not found"
+        assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text in \
+               self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text, \
+               "Message about adding product not found"
 
     def should_be_message_total_price(self):
-        assert self.browser.find_element(*ProductPageLocators.MESSAGE_PRICE).text ==  \
+        assert self.browser.find_element(*ProductPageLocators.MESSAGE_PRICE).text == \
                self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text, "Message about total price not found"
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
 
-
-
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message hasn't disappeared."

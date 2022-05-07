820de9b54e36b40e005399e9127b27be11abbe46
adam-eden <andrei1foto@gmail.com>
2022-05-07 13:45:13 +0600

    fixed checking for messages

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,11 +8,12 @@ class ProductPage(BasePage):
         button.click()
 
     def should_be_message_add_product(self):
-        assert self.is_element_present(*ProductPageLocators.MESSAGE_ADD), "Message about adding product not found"
+        assert "Coders at Work" == self.browser.find_element(*ProductPageLocators.MESSAGE_ADD).text, \
+            "Message about adding product not found"
 
     def should_be_message_total_price(self):
-        assert self.is_element_present(*ProductPageLocators.MESSAGE_PRICE) ==  \
-               self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), "Message about total price not found"
+        assert self.browser.find_element(*ProductPageLocators.MESSAGE_PRICE).text ==  \
+               self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text, "Message about total price not found"
 
 
 

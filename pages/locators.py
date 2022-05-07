ca4dd05146e3d5bba17d9e9060e6e24a67d78dd9
adam-eden <andrei1foto@gmail.com>
2022-05-07 13:44:00 +0600

    changed actual css selector for Message_ADD

--- pages/locators.py
+++ pages/locators.py
@@ -12,7 +12,7 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    MESSAGE_ADD = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
+    MESSAGE_ADD = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
     MESSAGE_PRICE = (By.CSS_SELECTOR, "#messages p:nth-child(1) > strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, "#content_inner .price_color")
 

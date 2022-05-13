af080d11519f8d3b6abaac884037aa980f65461f
adam-eden <andrei1foto@gmail.com>
2022-05-13 19:56:44 +0600

    added tests for registered users

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
+from pages.login_page import LoginPage
 import time
 import pytest
 
@@ -86,3 +87,29 @@ def test_guest_cant_see_message_an_empty_cart(browser):
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_message_an_empty_cart()
+
+
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login"
+        login_page = LoginPage(browser, link)
+        login_page.open()
+        login_page.register_new_user(str(time.time()) + "@mymail.com", str(time))
+        login_page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        time.sleep(2)
+        page.should_be_message_add_product()
+        page.should_be_message_total_price()

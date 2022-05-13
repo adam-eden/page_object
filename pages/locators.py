d15d38637af21a88575e054e4f1c9ef15a4f507a
adam-eden <andrei1foto@gmail.com>
2022-05-13 18:19:24 +0600

    added locators for BasketPage

--- pages/locators.py
+++ pages/locators.py
@@ -1,21 +1,24 @@
 from selenium.webdriver.common.by import By
 
 
-class BasePageLocators():
+class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    VIEW_BASKET_BTN = (By.CSS_SELECTOR, ".btn-group")
 
 
-class LoginPageLocators():
+class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
 
-class ProductPageLocators():
+class ProductPageLocators:
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1) strong")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main > h1")
     MESSAGE_PRICE = (By.CSS_SELECTOR, "#messages p:nth-child(1) > strong")
     PRODUCT_PRICE = (By.CSS_SELECTOR, "#content_inner .price_color")
 
 
+class BasketPageLocators:
+    EMPTY_BASKET = (By.CSS_SELECTOR, "#content_inner")

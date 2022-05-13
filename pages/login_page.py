1e353cb09350ed24b2a7b0dc23992d62692b0087
adam-eden <andrei1foto@gmail.com>
2022-05-13 19:57:29 +0600

    added user registration

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,6 +8,16 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
+    def register_new_user(self, email, password):
+        mail_input = self.browser.find_element(*LoginPageLocators.REG_EMAIL)
+        mail_input.send_keys(email)
+        pass_input1 = self.browser.find_element(*LoginPageLocators.REG_PASSWORD)
+        pass_input1.send_keys(password)
+        pass_input2 = self.browser.find_element(*LoginPageLocators.REPEAT_PASSWORD)
+        pass_input2.send_keys(password)
+        button_reg = self.browser.find_element(*LoginPageLocators.BUTTON_REG)
+        button_reg.click()
+
     def should_be_login_url(self):
         assert "login" in self.browser.current_url, "Login URL is not presented"
 
